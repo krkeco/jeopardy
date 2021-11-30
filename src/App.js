41797bc58de4830c13ce5aee58c3cf1e81c7f63b
krkeco <=>
2021-11-30 09:37:05 -0800

    scoreboard

--- src/App.js
+++ src/App.js
@@ -3,11 +3,34 @@ import logo from './logo.svg';
 import './App.css';
 import {titles, data} from './christmas2021'
 
+const ScoreTile = () =>{
+  const [score, setScore] = useState(0)
+  return (
+    <div className='scoretile'>
+      <input></input>
+      score:{score}
+      <button onClick={()=>setScore(score+100)} >+</button>
+      <button onClick={()=>setScore(score-100)} >-</button>
+    </div>
+  )
+}
+
+const ScoreBoard = () => {
+  const [teams, setTeams] = useState([1])
+  return(<div>
+    <button onClick={()=>setTeams([...teams,1])} >+</button>
+      <button onClick={()=>{
+        const newTeams = teams.slice(0,-1)
+        setTeams([...newTeams])}} >-</button>
+    {teams.map(team => <ScoreTile/>)}
+  </div>)
+}
+
 const QCard = ({data, value}) => {
   const [showQ, setShowQ] = useState(false)
   const [showA, setShowA] = useState(false)
   return(<div>{showQ && <div className='question'>
-    <button onClick={()=>{setShowQ(false)}}>X</button>
+    <button onClick={()=>{setShowQ(false); setShowA(false)}}>X</button>
     <button onClick={()=>{setShowA(!showA)}}>answer</button>
       {data.question}
       {showA && data.answer}
@@ -30,6 +53,9 @@ function App() {
          return <QCard value={index%5*100+100} data={row}/>
        })}</div>)
      })}
+     <div className='col'>
+       <ScoreBoard/>
+     </div>
     </div>
   );
 }
--- src/christmas2021.js
+++ src/christmas2021.js
@@ -1,119 +1,180 @@
 export const titles = [
     "The Globe",
-    "MODERN CULTURE"
+    "MODERN CULTURE",
+    "INVENTIONS",
+    "MUSIC",
+    "KOREAN WAR"
 ]
 export const data = [
 [
-    {
-        question:"What is the 7th planet from the Sun?",
-        answer: "Uranus",
-        kquestrion:"태양에서 7번째 행성은?",
-        kanswer: "천왕성",
-    },
-    {
-        question: "How many time zones are in Canada?",      
-        answer: "6",
-        kquestion: "캐나다에는 몇 개의 시간대가 있습니까?",
-        kanswer: "6"
-    }
+{
+question:"What is the 7th planet from the Sun?",
+answer: "Uranus",
+kquestrion:"태양에서 7번째 행성은?",
+kanswer: "천왕성",
+},
+{
+question: "How many time zones are in Canada?",      
+answer: "6",
+kquestion: "캐나다에는 몇 개의 시간대가 있습니까?",
+kanswer: "6"
+},
+{
+question:"What planet in our solar system has the most gravity? ",
+answer:"Jupiter",
+kquestion:"태양계에서 중력이 가장 큰 행성은? ",
+kanswer:"목성"
+},
+{
+question:"What country has the largest surface area: Australia, Brazil, or India? ",
+answer:"Brazil",
+kquestion:"호주, 브라질 또는 인도 중 가장 큰 표면적이 있는 국가는 어디입니까? ",
+kanswer:"브라질"
+},
 
-// 
-// 
-
-// What planet in our solar system has the most gravity? Jupiter
-// 태양계에서 중력이 가장 큰 행성은? 목성
-
-// What country has the largest surface area: Australia, Brazil, or India? Brazil
-// 호주, 브라질 또는 인도 중 가장 큰 표면적이 있는 국가는 어디입니까? 브라질
-
-
-// What’s the diameter of Earth (to the nearest 1,000 miles)? 8,000 miles
-// 지구의 지름은 얼마입니까(가장 가까운 1,000마일까지)? 8,000마일
+{
+question:"What’s the diameter of Earth (to the nearest 1,000 miles)? ",
+answer:"8,000 miles",
+kquestion:"지구의 지름은 얼마입니까(가장 가까운 1,000마일까지)? ",
+kanswer:"8,000마일"
+},
 
 ],
 // MODERN CULTURE
 [
-    {
-        question:"In which sport is the Vince Lombardi Trophy awarded?",
-        answer: "American Football",
-        kquestion:"빈스 롬바르디 트로피는 어떤 스포츠에서 수여됩니까?",
-        kanswer: "미식 축구"
-    }
+{
+question:"In which sport is the Vince Lombardi Trophy awarded?",
+answer: "American Football",
+kquestion:"빈스 롬바르디 트로피는 어떤 스포츠에서 수여됩니까?",
+kanswer: "미식 축구"
+},
+{
+question:"Who directed the award-winning Korean film, “Parasites” movie? ",
+answer:"Bong Joon-ho",
+kquestion:"한국영화 '기생충' 감독은? ",
+kanswer:"봉준호"
+},
+{
+question:"How many fingers does Mickey Mouse have? ",
+answer:"8 fingers ",
+kquestion:"미키마우스의 손가락은 몇 개입니까? ",
+kanswer:"8개의 손가락"
+},
+{
+question:"Who is the youngest BTS member? ",
+answer:"Jeon Jungkook",
+kquestion:"방탄소년단의 막내 멤버는? ",
+kanswer:"전정국"
+},
+{
+question:"Name 3 of the 4 most sought-after precious stones. ",
+answer:"Diamond, Sapphire, Emerald, Ruby",
+kquestion:"가장 많이 찾는 4개의 보석 중 3개의 이름을 지정하십시오. ",
+kanswer:"다이아몬드, 사파이어, 에메랄드, 루비"
+},
 
-// Who directed the award-winning Korean film, “Parasites” movie? Bong Joon-ho
-// 한국영화 '기생충' 감독은? 봉준호
-
-// How many fingers does Mickey Mouse have? 8 fingers 
-// 미키마우스의 손가락은 몇 개입니까? 8개의 손가락
-
-// Who is the youngest BTS member? Jeon Jungkook
-// 방탄소년단의 막내 멤버는? 전정국
-
-// Name 3 of the 4 most sought-after precious stones. Diamond, Sapphire, Emerald, Ruby
-// 가장 많이 찾는 4개의 보석 중 3개의 이름을 지정하십시오. 다이아몬드, 사파이어, 에메랄드, 루비
 
 
-
-]
-
+],
+[
 
 
 // INVENTIONS
+{
+question:"Who invented the rabies vaccination? ",
+answer:"Louis Pasteur ",
+question:"누가 광견병 예방 접종을 발명 했습니까? ",
+answer:"루이 파스퇴르",},
+{
+question:"What scientist formulated the Laws of Motion? ",
+answer:"Sir Isaac Newton",
+question:"운동 법칙을 공식화한 과학자는? ",
+answer:"아이작 뉴턴 경",},
+{
+question:"Who invented the Korean alphabet? ",
+answer:"Sejong, fourth king of the Chosŏn dynasty",
+question:"누가 한글을 발명했습니까? ",
+answer:"조선의 4대 왕 세종",},
+{
+question:"Who introduced the idea of the ‘assembly line’ thus allowing mass car manufacturing? ",
+answer:"Henry Ford",
+question:"자동차 대량 생산이 가능한 '조립 라인'은 누가 도입했습니까? ",
+answer:"헨리 포드",},
+{
+question:"Who invented the crown bottle or cork caps? ",
+answer:"William Painter n 1892",
+question:"누가 왕관 병이나 코르크 마개를 발명했습니까? ",
+answer:"1892년 윌리엄 페인터",
+}
 
-// Who invented the rabies vaccination? Louis Pasteur 
-// 누가 광견병 예방 접종을 발명 했습니까? 루이 파스퇴르
-
-// What scientist formulated the Laws of Motion? Sir Isaac Newton
-// 운동 법칙을 공식화한 과학자는? 아이작 뉴턴 경
-
-// Who invented the Korean alphabet? Sejong, fourth king of the Chosŏn dynasty
-// 누가 한글을 발명했습니까? 조선의 4대 왕 세종
-
-// Who introduced the idea of the ‘assembly line’ thus allowing mass car manufacturing? Henry Ford
-// 자동차 대량 생산이 가능한 '조립 라인'은 누가 도입했습니까? 헨리 포드
-
-// Who invented the crown bottle or cork caps? William Painter n 1892
-// 누가 왕관 병이나 코르크 마개를 발명했습니까? 1892년 윌리엄 페인터
-
-
-
+],[
 
 // MUSICAL INSTRUMENTS
-
-// How many valves does a trumpet have? 3
-// 트럼펫에는 몇 개의 밸브가 있습니까? 3
-
-// How many strings does a standard ukulele have? 4 
-// 우쿨렐레의 현은 몇 개입니까? 4
-
-// How many notes are in a chord? 3 
-// 코드에 몇 개의 음표가 있습니까? 3
-
-// Which came first: the piano or the organ? The organ 
-// 피아노와 오르간 중 어느 악기가 먼저 만들어졌습니까? 오르간
-
-// Name 3 of the 4 principal instruments in a string quartet. Violin. Bass Fiddle/2nd Violin, Cello, Viola
-// 현악 4중주의 4가지 주요 악기 중 3가지가 무엇입니까? 바이올린. 베이스 바이올린/제2 바이올린, 첼로, 비올라
-
+{
+question:"How many valves does a trumpet have? ",
+answer:"3",
+kquestion:"트럼펫에는 몇 개의 밸브가 있습니까? ",
+kanswer:"3"
+},
+{
+question:"How many strings does a standard ukulele have? ",
+answer:"4 ",
+kquestion:"우쿨렐레의 현은 몇 개입니까? ",
+kanswer:"4"
+},
+{
+question:"How many notes are in a chord? ",
+answer:"3 ",
+kquestion:"코드에 몇 개의 음표가 있습니까? ",
+kanswer:"3"
+},
+{
+question:"Which came first: the piano or the organ? ",
+answer:"The organ ",
+kquestion:"피아노와 오르간 중 어느 악기가 먼저 만들어졌습니까? ",
+kanswer:"오르간"
+},
+{
+question:"Name 3 of the 4 principal instruments in a string quartet. ",
+answer:"Violin. Bass Fiddle/2nd Violin, Cello, Viola",
+kquestion:"현악 4중주의 4가지 주요 악기 중 3가지가 무엇입니까? 바이올린. ",
+kanswer:"베이스 바이올린/제2 바이올린, 첼로, 비올라"
+},
+],[
 // KOREAN WAR
+{
+question:"Korea was split in half after which war? ",
+answer:"World War II",
+kquestion:"한국이 반으로 갈라진 전쟁은? ",
+kanswer:"제2차 세계 대전"
+},
+{
+question:"The Korean War began on which date? ",
+answer:"June 25 1950",
+kquestion:"한국 전쟁이 시작된 날짜는? ",
+kanswer:"1950년 6월 25일"
+},
+{
+question:"Who was the US president when the US entered the Korean War? ",
+answer:"President Harry S. Truman",
+kquestion:"미국이 한국전쟁에 참전했을 때 미국 대통령은 누구였습니까? ",
+kanswer:"해리 S. 트루먼 대통령"
+},
+{
+question:"When did Syngman Rhee’s (1st South Korean president) presidency begin and end? Provide the exact years. ",
+answer:"1948 to 1960",
+kquestion:"이승만(대한민국 초대 대통령)의 임기는 언제 시작되고 끝났습니까? 정확한 연도를 제공하십시오. ",
+kanswer:"1948년 ~ 1960년"
+},
+{
+question:"North and South Korea are still separated by a fortified demilitarized zone (DMZ). How wide is the DMZ? ",
+answer:"2.5-mile-wide or 2 km-wide",
+kquestion:"남북한은 여전히 ​​DMZ(비무장 지대)로 분리되어 있습니다. DMZ의 넓이는? ",
+kanswer:"너비 2.5마일 또는 너비 2km"
+},
 
-// Korea was split in half after which war? World War II
-// 한국이 반으로 갈라진 전쟁은? 제2차 세계 대전
 
-// The Korean War began on which date? June 25 1950
-// 한국 전쟁이 시작된 날짜는? 1950년 6월 25일
-
-// Who was the US president when the US entered the Korean War? President Harry S. Truman
-// 미국이 한국전쟁에 참전했을 때 미국 대통령은 누구였습니까? 해리 S. 트루먼 대통령
-
-// When did Syngman Rhee’s (1st South Korean president) presidency begin and end? Provide the exact years. 1948 to 1960
-// 이승만(대한민국 초대 대통령)의 임기는 언제 시작되고 끝났습니까? 정확한 연도를 제공하십시오. 1948년 ~ 1960년
-
-// North and South Korea are still separated by a fortified demilitarized zone (DMZ). How wide is the DMZ? 2.5-mile-wide or 2 km-wide
-// 남북한은 여전히 ​​DMZ(비무장 지대)로 분리되어 있습니다. DMZ의 넓이는? 너비 2.5마일 또는 너비 2km
-
-
-
+]
 
 // DOUBLE-WHAMMY or (INDIVIDUAL POP-UP QUESTIONS)
 
